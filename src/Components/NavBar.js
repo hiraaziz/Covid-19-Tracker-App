ee74bc16b10eb40b8da5c11379547e44a8e9a6c3
hiraaziz <hiraaziz1998@gmail.com>
2021-05-22 15:20:42 -0400

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
@@ -11,6 +10,10 @@ import SearchIcon from '@material-ui/icons/Search';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    
+  },
+  color:{
+    backgroundColor: 'rgba(129,185,191,0.5)'
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -67,8 +70,8 @@ export default function NavBar() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
+    <div className={classes.root} >
+      <AppBar position="static" className={classes.color}>
         <Toolbar>
           <Typography className={classes.title} variant="h6" noWrap>
             Covid19 Tracker
