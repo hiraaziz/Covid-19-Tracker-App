a39803693cd01a7cb5bf8797025c2cb02046450d
hiraaziz <hiraaziz1998@gmail.com>
2021-05-22 15:17:01 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, {useState} from 'react';
 import NavBar from './Components/NavBar'
 import InfoPanel from './Components/InfoPanel'
+import Footer from './Components/Footer'
 import './App.css';
 
 function App() {
+
+  const screenConfig = useState(0);
   return (
-    <div>
+   
+    <div className="banner">
       <NavBar/>
-      <InfoPanel/>
+      <InfoPanel currentScreen={screenConfig[0]}/>
+      <Footer screenConfig={screenConfig}/>
     </div>
+  
   );
 }
 
