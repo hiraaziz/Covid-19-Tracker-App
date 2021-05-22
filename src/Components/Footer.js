55663ae4648589599472e74c27228f6eea5d593d
hiraaziz <hiraaziz1998@gmail.com>
2021-05-22 15:20:11 -0400

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,32 +5,36 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 const useStyles = makeStyles({
   root: {
     position: 'absolute',
     bottom: 0,
     right:0,
-    left:0
+    left:0,
+    backgroundColor: 'rgba(129,185,191,0.7)',
   },
+  
 });
 
-export default function Footer() {
+export default function Footer({screenConfig}) {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  // const [value, setValue] = React.useState(0);
 
   return (
     <BottomNavigation
-      value={value}
+      value={screenConfig[0]}
       onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
+        console.log(screenConfig[0])
+        screenConfig[1](newValue);
+      } }
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="Global state" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Country state" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Global state" icon={<ReorderIcon />} />
+      <BottomNavigationAction label="Country state" icon={<ReorderIcon />} />
+      
     </BottomNavigation>
   );
 }
